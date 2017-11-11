9c1d4fce3205897d5b17deba61b8897494abca30
ssan9 <perrsy@gmail.com>
2017-11-11 15:47:06 -0500

    Unit Testing for fizzBuzzer function in this file

---
+++ test/test-fizz-buzzer.js
@@ -0,0 +1,76 @@
+const should = require('chai').should();
+
+const fizzBuzzer = require('../fizzBuzzer');
+
+// unit tests for our `fizzBuzzer` function
+describe('fizzBuzzer', function() {
+
+	// test the normal case
+	it('should return \'fizz-buzz\' if the number is divisible by 15', function() {
+		//range of normal inputs
+		const normalCases = [
+			{num: 30, expected: 'fizz-buzz'},
+			{num: 60, expected: 'fizz-buzz'},
+			{num: 105, expected: 'fizz-buzz'}
+		];
+	});
+
+	it('should return \'buzz\' if the number is divisible by 5', function() {
+
+		const normalCases = [
+			{num:10, expected: 'buzz'},
+			{num :35, expected: 'buzz'},
+			{num: 50, expected: 'buzz'}
+		];
+	});
+
+	it('should return \'fizz\' if the number is divisible by 3', function() {
+
+		const normalCases = [
+			{num: 6, expected: 'fizz'},
+			{num: 18, expected: 'fizz'},
+			{num: 78, expected: 'fizz'}
+		];
+	// for each input, `fizzBuzzer` should 
+	// produce the expected value
+		normalCases.forEach(function(input) {
+			const answer = fizzBuzzer(input.num);
+			answer.should.equal(input.expected);
+		});
+	});
+
+	it('should return number if not mult of 3 or 5', function() {
+    
+    const randomNumbers = [
+    	1, 
+    	2,
+    	4, 
+    	7
+    ];
+
+    randomNumbers.forEach(function(input) {
+      fizzBuzzer(input).should.equal(input);
+    });
+  });
+
+	it('should raise error if args not numbers', function() {
+	// range of bad inputs where number is not given
+		const badInputs = [
+			null,
+			undefined,
+			'10',
+			'num',
+			{},
+			[],
+			true,
+			false
+		];
+	// prove that an error is raised for bad inputs
+		badInputs.forEach(function(input) {
+			(function() {
+				fizzBuzzer(input)
+			}).should.throw(Error);
+		});
+	});
+});
+
